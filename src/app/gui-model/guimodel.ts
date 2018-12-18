126e72995098589938c9651b399b915ede9c7aab
Lukas Roth <lukas.roth@hotmail.com>
2018-12-18 23:34:28 +0100

    updated

--- src/app/gui-model/guimodel.ts
+++ src/app/gui-model/guimodel.ts
@@ -6,7 +6,7 @@ export class GuiModel {
 
     private _guiModel = {
         "application": {
-            "title": "Requirements Engineering Friend Tracker",
+            "title": "Lukas Roth s Friend Tracker",
             "formList": [
                 {
                     "id": "FriendForm",
@@ -27,6 +27,13 @@ export class GuiModel {
                             "width": 1,
                             "required": true
                         },
+						{
+						 "id": "nickname",
+						 "type": "text",
+						 "name": "Nickname",
+						 "width": 2,
+						 "required": true
+						},
                         {
                             "id":   "location",
                             "type": "autocomplete",
@@ -60,7 +67,15 @@ export class GuiModel {
                         {
                             "type": "okButton",
                             "name": "Ok"
-                        }
+                        },
+						{
+							 "id": "group",
+							 "type": "autocomplete",
+							 "name": "Group",
+							 "url": "/group",
+							 "form": "GroupForm",
+							 "width": 2
+						},
                     ]
                 },
                 {
@@ -118,12 +133,40 @@ export class GuiModel {
                         }
                     ]
                 },
+				{
+					 "id": "GroupForm",
+					 "title": "Group",
+					 "url": "/group",
+					 "formFieldList": 
+					 [
+						 {
+							 "id": "name",
+							 "type": "text",
+							 "name": "GroupName",
+							 "width": 2,
+							 "required": true
+						},
+						{
+							"type": "deleteButton",
+							"name": "Delete"
+						},
+						{
+							"type": "cancelButton",
+							"name": "Cancel"
+						},
+						{
+							"type": "okButton",
+							"name": "Ok"
+						}
+					]
+				}
             ],
             "pageList": [
                 {
                     "id": "mainmenu",
                     "name": "MainMenu",
-                    "elementList": [
+                    "elementList": 
+					[
                         {
                             "type": "button",
                             "name": "Friends",
@@ -138,11 +181,19 @@ export class GuiModel {
                             "color": "yellow",
                             "page": "locationspage",
                         },
+						{
+						 "type": "button",
+						 "name": "Groups",
+						 "icon": "fa-weixin",
+						 "color": "wisteria",
+						 "page": "groupspage",
+						},
                     ]
                 },
                 {
                     "id": "friendspage",
-                    "elementList": [
+                    "elementList": 
+					[
                         {
                             "type": "backbutton",
                         },
@@ -169,7 +220,8 @@ export class GuiModel {
                 },
                 {
                     "id": "locationspage",
-                    "elementList": [
+                    "elementList": 
+					[
                         {
                             "type": "backbutton",
                         },
@@ -195,6 +247,34 @@ export class GuiModel {
                         },
                     ]
                 },
+				{
+					 "id": "groupspage",
+					 "elementList": 
+					 [
+						 {
+							"type": "backbutton",
+						 },
+						 {
+							 "type": "newButton",
+							 "name": "NewGroup",
+							 "icon": "fa-weixin",
+							 "color": "green",
+							 "form": {
+								"form": "GroupForm"
+							 }
+						 },
+						 {
+							"type": "list",
+							"icon": "fa-weixin",
+							"color": "wisteria",
+							"search": true,
+							"url": "/group",
+							"form": {
+								"form": "GroupForm"
+							}
+						},
+					 ]
+				}
             ]
         }
     };
